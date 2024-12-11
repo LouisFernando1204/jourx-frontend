d3169864b54f172d36e29c370dd3f657f537f7ed
Louis Fernando <ferlouis55@gmail.com>
2024-12-11 09:55:40 +0700

    Prepare for testing the Mailer feature from Laravel ; Get the sha-256 key from debub.keystore and store it to assetlinks.json (Rumahweb hosting)

---
+++ lib/view/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final String username;
+
+  const HomePage({super.key, required this.username});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    String username = widget.username;
+    return Scaffold(
+      backgroundColor: Colors.blue[50],
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: Text("Home Page", style: TextStyle(color: Colors.white)),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Registration Successful!",
+                style: TextStyle(color: Colors.white)),
+            Text("Username: ${username}",
+                style: TextStyle(color: Colors.white)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/register_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: Text("Register Page", style: TextStyle(color: Colors.white)),
+      ),
+      body: Container(
+          child: Text("This is Registration Page",
+              style: TextStyle(color: Colors.blue))),
+    );
+  }
+}
