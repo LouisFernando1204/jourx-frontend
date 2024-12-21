b58d9fbf7700b69fd81568236f53edfa070cd15c
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2024-12-22 04:54:04 +0700

    Journaling and voice recognition

--- lib/view/pages/pages.dart
+++ lib/view/pages/pages.dart
@@ -1,10 +1,13 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:jourx/view_model/journaling_viewmodel.dart';
 import 'package:provider/provider.dart';
 import 'package:jourx/view_model/login_viewmodel.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart'; 
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 part 'login_page.dart';
 part 'register_page.dart';
+part 'journaling_page.dart';
---
+++ lib/view_model/journaling_viewmodel.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class JournalingViewmodel with ChangeNotifier {
+  
+}
